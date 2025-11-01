13a37d207e269261f78323c3ab1bd81e7b3b0258
RobertoTrue <91092709+RobertoTrue@users.noreply.github.com>
2025-10-31 21:29:17 -0400

    Add files via upload

---
+++ ecuacicion-matematica/ecuacicion-matematica/Ecuacion.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace ecuacicion_matematica
+{
+    public class Ecuacion
+    {
+        double a;
+        double b;
+        double c;
+        public Ecuacion(double a ,double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public void CalcularSoluciones()
+        {
+            double dis = this.b * this.b - 4 * this.a * this.c;
+
+            if (dis > 0)
+            {
+                double raiz1 = (-b + Math.Sqrt(dis)) / (2 * this.a);
+                double raiz2 = (-b - Math.Sqrt(dis)) / (2 * this.a);
+                Console.WriteLine("Resultado > 0");
+                Console.WriteLine("Primera Raiz: " + raiz1);
+                Console.WriteLine("Segunda Raiz: " + raiz2);
+            }
+            else if (dis == 0)
+            {
+                double raiz = -this.b / (2 * this.a);
+                Console.WriteLine("Resultado " + raiz);
+            }
+            else
+            {
+                Console.WriteLine("Las soluciones son muy complejas.");
+            }
+        }
+    }
+}
---
+++ ecuacicion-matematica/ecuacicion-matematica/Program.cs
@@ -0,0 +1,22 @@
+﻿using ecuacicion_matematica;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Ingrese el valor de a:");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el valor de b:");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el valor de c:");
+        double c = Convert.ToDouble(Console.ReadLine());
+
+        // Creo un objecto para utilizarlo para llamar el metodo
+        Ecuacion ecuacion = new Ecuacion(a, b, c);
+
+        // Llamo al metodo para calcular las soluciones
+        ecuacion.CalcularSoluciones();
+    }
+}
